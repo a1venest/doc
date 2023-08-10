7160c92a18b4b4ac9eab8578e0e8a393c4227b26
Aivenest <aivenest@gmail.com>
2023-08-10 22:17:28 +0500

    1.0.2

--- modules/core/src/com/company/doc/listeners/DocumentTypeCodeCreator.java
+++ modules/core/src/com/company/doc/listeners/DocumentTypeCodeCreator.java
@@ -19,7 +19,7 @@ public class DocumentTypeCodeCreator implements BeforeInsertEntityListener<Docum
     @Override
     public void onBeforeInsert(DocumentType entity, EntityManager entityManager) {
 
-        entity.setCode("" + uniqueNumbersAPI.getNextNumber("orderId"));
+        entity.setCode("" + uniqueNumbersAPI.getNextNumber("Code"));
 
 
     }
